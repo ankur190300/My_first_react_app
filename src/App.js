62792f3cff9947dc52228d69a311377243bce206
Ankur <ankur190300@gmail.com>
2020-07-08 09:40:42 +0530

    first commit:setup the default react app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        DJ ankur in the house boiiis
+
         </p>
-        <a
+       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
---
+++ src/component/greet.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Greet(){
+
+    return <h1>This app belongs to ankur tiwari copyrights reserved</h1>
+}
